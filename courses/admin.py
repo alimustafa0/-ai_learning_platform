7c5d2802f93fb8907605fea8a223eec6024c4d92
alimustafa0 <alimustafa010686@gmail.com>
2026-02-03 14:14:45 +0200

    Add Django admin with inline modules and lessons

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Course, Module, Lesson
 
-# Register your models here.
+
+class LessonInline(admin.TabularInline):
+    model = Lesson
+    extra = 1
+    ordering = ("order",)
+
+
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ("title", "course", "order")
+    ordering = ("course", "order")
+    inlines = [LessonInline]
+
+
+class ModuleInline(admin.TabularInline):
+    model = Module
+    extra = 1
+    ordering = ("order",)
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("title", "is_published", "created_at")
+    list_filter = ("is_published",)
+    search_fields = ("title",)
+    inlines = [ModuleInline]
